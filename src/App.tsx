34da4156d816b9ad4c6b8973b47d1b97ef151f78
Sergiy Rudenko <sergiy@powertree.io>
2020-04-26 16:46:19 -0400

    added formik plus yup validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
-function App() {
+const SigninSchema = Yup.object().shape({
+  email: Yup.string().email("Must be a valid email").required("Required"),
+  password: Yup.string()
+    .required("Required")
+    .min(8, "Must be at least 8 characters"),
+});
+
+const handleSubmit = (values: any) => {
+  console.log(`email: ${values.email}, password: ${values.password}`);
+};
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hi there</h1>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={SigninSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ dirty, handleReset, isValid }) => (
+          <Form>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <Field autocomplete="off" name="email" />
+              <ErrorMessage name="email" />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <Field autocomplete="off" name="password" />
+              <ErrorMessage name="password" />
+            </div>
+            <button disabled={!isValid || !dirty} type="submit">
+              SUBMIT
+            </button>
+            <button onClick={handleReset} type="button">
+              Reset
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
 
 export default App;
